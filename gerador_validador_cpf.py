de9a87ef1676bf7252fce8f880c6bc231d61ff3c
OpsRoubeiSuaVaga <zmarlon.zevallos@gmail.com>
2023-04-14 16:29:04 -0300

    ultimos toques

--- gerador_validador_cpf.py
+++ gerador_validador_cpf.py
@@ -3,47 +3,45 @@ from random import randint
 
 while True:
     print("Escolha uma opção:")
-    opcao = input("[1]gerar um cpf [2]verificar a validade de um cpf: ")
+    opcao = input("[1]Gerar um cpf [2]Verificar a validade de um cpf: ")
     if opcao == "1":
         soma_nove_dig = 0
         soma_dez_dig = 0
 
         n = 0
-        numeros = []
+        numeros = ""
         while n < 10:
-            numeros.append(n)
+            numeros += str(n)
             n += 1
 
         i = 0
-        cpf = []
+        cpf = ""
         while i < 9:
             num_sortido = randint(0 , 9)
-            cpf.append(num_sortido)
+            cpf += str(num_sortido)
             i += 1
 
         multiplo_regressivo = 10
         for digito in cpf:
-            soma_nove_dig += digito * multiplo_regressivo
+            soma_nove_dig += int(digito) * multiplo_regressivo
             multiplo_regressivo -= 1
 
         vezes_dez = soma_nove_dig * 10
         resto_por_onze = vezes_dez % 11
-
         decimo_digito = resto_por_onze if resto_por_onze <= 9 else 0
-        cpf.append(decimo_digito)
+        
 
         multiplo_regressivo = 11
         for digito in cpf:
-            soma_dez_dig += digito * multiplo_regressivo
+            soma_dez_dig += int(digito) * multiplo_regressivo
             multiplo_regressivo -= 1
 
         vezes_dez = soma_dez_dig * 10
         resto_por_onze = vezes_dez % 11
         ultimo_digito = resto_por_onze if resto_por_onze <= 9 else 0
-        cpf.append(ultimo_digito)
 
         os.system("clear")
-        print("CPF gerado aleatoriamente:", *cpf)
+        print(f"CPF gerado aleatoriamente: {cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{decimo_digito}{ultimo_digito}")
 
     elif opcao == "2":
         os.system("clear")
@@ -52,13 +50,17 @@ while True:
         arroz = True
         while arroz:
             try:
-                cpf = input("Digite o cpf: ")
+                cpf = input("Digite o cpf(somente números): ") 
                 testando_int = int(cpf)
-                nove_digitos_cpf = cpf[:9]
+                cpf_repetido_true = cpf == str(cpf[0] * (len(cpf)))
+                if cpf_repetido_true:
+                    print("NÃO REPITA TODOS OS NÚMEROS!")
+                    continue
                 arroz = False
             except ValueError:
                 print("Digite somente números!")
-
+        
+        nove_digitos_cpf = cpf[:9]
         multiplo_regressivo = 10
         for digito in nove_digitos_cpf:
             soma_nove_dig += int(digito) * multiplo_regressivo
